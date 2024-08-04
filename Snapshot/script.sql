09a12694c34082cef8c17cade1cc7fe4d43d5707
JaeDragon <leejy0068@gmail.com>
2024-08-04 19:56:42 +0900

    Update script.sql

--- Snapshot/script.sql
+++ Snapshot/script.sql
@@ -20,10 +20,12 @@ BGSAVE
 -- RDB 파일 로드 방식
 -- Redis 서버가 시작될 때 RDB 파일을 로드
 -- Redis 서버가 RDB 파일을 읽어 Redis 데이터베이스를 복원
+-- RDB 파일을 로드하는 방법은 Redis 서버가 비정상적으로 종료되었을 때 데이터를 복구하거나 데이터베이스를 복원할 때 사용
 redis-server --rdb <rdb_file_path>
 
 -- 방법 2.
 -- RDB 파일 Import 방식
 -- Redis 서버가 이미 실행 중일 때 RDB 파일을 적용
 -- Redis 서버에 RDB 파일을 import하는 방법은 RDB 파일을 직접 Redis 데이터베이스에 적용
+-- Redis 서버에 RDB 파일을 import하는 방법은 Redis 서버의 데이터를 다른 환경으로 이전하거나 데이터를 공유할 때 사용
 redis-cli --rdb <rdb_file_path>
